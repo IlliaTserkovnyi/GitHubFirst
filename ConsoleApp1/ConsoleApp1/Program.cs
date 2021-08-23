b1bd5e76c52991cd9860db9b44f8f40aef6633f1
IlliaTserkovnyi <Illia.Tserkovnyi@teaminternational.com>
2021-08-23 11:54:45 +0300

    got new branch with PalindromeCheck

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            bool flag = true;
+            while (flag)
+            {
+                Console.Clear();
+                string originalWord = Console.ReadLine();
+                Console.WriteLine($"Is word {originalWord} palindrome? True/False \n" + IsPalindrom(originalWord) + "\n---------");
+                Console.WriteLine("Try again? 1 - Yes. 2 - No.");
+                flag = Console.ReadLine() == "1";
+            }
+        }
+
+        static bool IsPalindrom(string word)
+        {
+            return word == new string(word.Reverse().ToArray());
         }
     }
 }
